d936364db07764a25f40e2851073d100d7c3fde2
Abhi Jain <devabhixda@gmail.com>
2025-11-14 11:17:11 -0800

    Lower minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.cactus.peyokeys"
-        minSdk = 30
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
